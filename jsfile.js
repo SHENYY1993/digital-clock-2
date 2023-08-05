9a19175be37393fa9ad41a0fbaa3f3f308db635e
SHENYY1993 <84908268+SHENYY1993@users.noreply.github.com>
2023-08-05 14:58:59 +0800

    Update jsfile.js
    change to current time

--- jsfile.js
+++ jsfile.js
@@ -3,24 +3,41 @@ var days, hours, minutes, seconds; // variables for time units
 
 var countdown = document.getElementById("tiles"); // get tag element
 
-getCountdown();
+getCurrentTime();
 
-setInterval(function () { getCountdown(); }, 1000);
+setInterval(function () { getCurrentTime(); }, 1000);
 
-function getCountdown(){
+// function getCountdown(){
+//
+//     // find the amount of "seconds" between now and target
+//     var current_date = new Date().getTime();
+//     var seconds_left = (target_date - current_date) / 1000;
+//
+//     days = pad( parseInt(seconds_left / 86400) );
+//     seconds_left = seconds_left % 86400;
+//
+//     hours = pad( parseInt(seconds_left / 3600) );
+//     seconds_left = seconds_left % 3600;
+//
+//     minutes = pad( parseInt(seconds_left / 60) );
+//     seconds = pad( parseInt( seconds_left % 60 ) );
+//
+//     // format countdown string + set tag value
+//     countdown.innerHTML = "<span>" + days + "</span><span>" + hours + "</span><span>" + minutes + "</span><span>" + seconds + "</span>";
+// }
+
+function getCurrentTime(){
 
     // find the amount of "seconds" between now and target
-    var current_date = new Date().getTime();
+    var current_date = new Date();
     var seconds_left = (target_date - current_date) / 1000;
 
-    days = pad( parseInt(seconds_left / 86400) );
-    seconds_left = seconds_left % 86400;
+    days = current_date.getDate();
 
-    hours = pad( parseInt(seconds_left / 3600) );
-    seconds_left = seconds_left % 3600;
+    hours = current_date.getHours();
 
-    minutes = pad( parseInt(seconds_left / 60) );
-    seconds = pad( parseInt( seconds_left % 60 ) );
+    minutes = current_date.getMinutes();
+    seconds = current_date.getSeconds();
 
     // format countdown string + set tag value
     countdown.innerHTML = "<span>" + days + "</span><span>" + hours + "</span><span>" + minutes + "</span><span>" + seconds + "</span>";
